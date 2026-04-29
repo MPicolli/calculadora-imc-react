a77278caff2785c5a39bdc940da325f38a8479a2
Matheus Picolli Ishibashi <matheus.ishibashi@gmail.com>
2026-04-29 18:20:01 -0300

    feat: implementa ImcCalc.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import ImcCalc from "./components/ImcCalc";
 
 function App() {
   return (
     <div>
       <div className="container">
-        <p>Calculadora de IMC</p>
+        <ImcCalc />
       </div>
     </div>
   );
