2bd4cf2fea3532daeef8f3addd4baf2c2fed3dd8
Matheus Picolli Ishibashi <matheus.ishibashi@gmail.com>
2026-05-06 18:56:32 -0300

    feat: Valida dígitos nos inputs Altura e Peso

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -13,6 +13,20 @@ const ImcCalc = () => {
     setWeight("");
   };
 
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "");
+  };
+
+  const handleHeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value);
+    setHeight(updatedValue);
+  };
+
+  const handleWeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value);
+    setWeight(updatedValue);
+  };
+
   return (
     <div id="calc-container">
       <h2>Calculadora de IMC</h2>
@@ -25,7 +39,7 @@ const ImcCalc = () => {
               name="height"
               id="height"
               placeholder="Exemplo 1,75"
-              onChange={(e) => setHeight(e.target.value)}
+              onChange={(e) => handleHeightChange(e)}
               value={height}
             />
           </div>
@@ -36,7 +50,7 @@ const ImcCalc = () => {
               name="weight"
               id="weight"
               placeholder="Exemplo 70,5"
-              onChange={(e) => setWeight(e.target.value)}
+              onChange={(e) => handleWeightChange(e)}
               value={weight}
             />
           </div>
